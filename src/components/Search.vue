473feebda517b109e33b4bb60aa660c2ebb85212
blade-01 <animashauntaofiq@yahoo.com>
2021-08-07 00:26:11 +0100

    Refactor: Added code link

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,7 +4,7 @@
       <div class="search-inner">
         <div class="flex-center">
           <button class="btn" @click="openSearch">Search for places</button>
-          <a href="https://github.com/blade-01" target="_blank">
+          <a href="https://github.com/blade-01/tiempo.git" target="_blank">
           <svg class="w-6 h-6 btn icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
           </a>
         </div>
